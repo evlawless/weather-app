2240e77f67c6b2fcd0e888ebcfb510da1d5a527f
evan <ev.lawless@gmail.com>
2018-09-16 17:53:11 -0400

    Got the data loading from OpenWeatherMap's api, now I need to display it. I also need to pull in the google maps API so you can search for weather in other places

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,98 @@
 import React from 'react';
 
+function kToC(deg) {
+	return deg - 273.15;
+}
+
+function cToF(deg) {
+	return (deg * 1.8 + 32);
+}
+
+function kToF(deg) {
+	return cToF(kToC(deg));
+}
+
+function cToK(deg) {
+	return deg + 273.15;
+}
+
+function fToC(deg) {
+	return (deg - 32) * (5 / 9);
+}
+
+function fToK(deg) {
+	return cToK(fToC(deg));
+}
+
 class Weather extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			location: "London, Ontario",
-			lat: 42.9849,
-			lng: 81.2453,
 			loading: false,
-			weatherData: {}
+			tempInfo: {},
+			weatherInfo: {}
 		};
 		this.updateWeatherData = this.updateWeatherData.bind(this);
+
+		this.locationChanged = this.locationChanged.bind(this);
+	}
+
+	componentDidMount() {
 		this.updateWeatherData();
 	}
 
 	updateWeatherData() {
+		const weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=London,CA&APPID=4708f504fd0931e98f15cc60ffc5cabe";
+		const forecasturl = "https://api.openweathermap.org/data/2.5/forecast?q=London,CA&APPID=4708f504fd0931e98f15cc60ffc5cabe";
+		const self = this;
+
+		$.getJSON([weatherurl], function (wdata) {
+			console.log(wdata);
+		
+			self.setState({
+				tempInfo: wdata.main,
+				weatherInfo: wdata.weather
+			});
+		});
+		
+	}
+
+	locationChanged(event) {
+
 	}
 
 	render() {
-		const dayStyle = { border: "1px solid #000", margin: 5 };
-		console.log(this.state.weatherData);
 
 		return (
-			<div className="container">
+			<div className="container weather-app">
 				<div className="row align-items-end">
-					<h3>showing weather info for: </h3>
-					&nbsp;
-					<h1>{this.state.location}</h1>
+
+					<h3>current weather for {this.state.location}</h3>
 				</div>
 				<div className="row">
-					<div>
+					<div className="col">Current Temp: {this.state.tempInfo.temp && kToC(this.state.tempInfo.temp).toFixed()}</div>
+					<div className="col">Today's High: {this.state.tempInfo.temp_max && kToC(this.state.tempInfo.temp_max).toFixed()}</div>
+					<div className="col">Today's low: {this.state.tempInfo.temp_min && kToC(this.state.tempInfo.temp_min).toFixed()}</div>
+				</div>
 
-					</div>
-				</div>
-				<div className="row my-5">
-					<div className="col-xs-6">
-						<div className="form-group">
-							<div className="input-group">
-								<div className="input-group-prepend">
-									<span className="input-group-text" id="">Location</span>
-								</div>
-								<input id="location_input" className="form-control" type="text" value={this.state.location} />
-								&nbsp;
-								<button className="btn ">Update</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				
+			</div>
+		);
+	}
+}
+
+class ForecastDay extends React.Component {
+	
+	render() {
+		return (
+			<div>
+				<h1>this.props.temperature</h1>
+				<p>
+					{this.props.high != null && this.props.high}<br/>
+					{this.props.low != null && this.props.low}
+				</p>
 			</div>
 		);
 	}
